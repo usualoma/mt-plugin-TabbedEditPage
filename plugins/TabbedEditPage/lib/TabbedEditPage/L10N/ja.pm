92b10a01e7a4a3bdd357eb3577b5744ba42e5abf
Taku AMANO <taku@taaas.jp>
2014-02-19 09:17:14 +0900

    Update plugin for MT6.

--- plugins/TabbedEditPage/lib/TabbedEditPage/L10N/ja.pm
+++ plugins/TabbedEditPage/lib/TabbedEditPage/L10N/ja.pm
@@ -5,22 +5,24 @@ use base 'TabbedEditPage::L10N::en_us';
 use vars qw( %Lexicon );
 
 %Lexicon = (
-	'toi-planning' => 'ToI企画',
-	'Make a tabbed editing screen.' => '編集ページをタブ化します',
-	'Updated' => '設定を更新しました',
-	'Disabled' => '利用しない',
-	'Enabled' => '利用する',
-	'Save' => '設定を保存',
-	'A new tab' => '新しいタブ',
-	'Add tab' => 'タブを追加',
-	'Enabled fields:' => '表示されるフィールド',
-	'Available fields:' => '表示可能なフィールド',
-	'Select blog' => 'ブログを選択してください',
-	'Copying' => 'コピーする',
-	'The blog has not been selected.' => 'ブログが選択されていません',
-	'Copying?' => 'コピーしてもよろしいですか？',
-	'Settings for Entry' => 'ブログ記事の編集ページの設定',
-	'Settings for Page' => 'ウェブページの編集ページの設定',
+    'Make a tabbed editing screen.' => '編集ページをタブ化します',
+    'Updated'                       => '設定を更新しました',
+    'Disabled'                      => '利用しない',
+    'Enabled'                       => '利用する',
+    'Save'                          => '設定を保存',
+    'A new tab'                     => '新しいタブ',
+    'Add tab'                       => 'タブを追加',
+    'Enabled fields:'               => '表示されるフィールド',
+    'Available fields:'             => '表示可能なフィールド',
+    'Select blog'                   => 'ブログを選択してください',
+    'Copying From Selected Blog' =>
+        '選択したブログから設定を複製する',
+    'The blog has not been selected.' =>
+        'ブログが選択されていません',
+    'Are you sure you want to copy from selected blog?' =>
+        '設定を複製して上書きしてもよろしいですか？',
+    'Settings for Entry' => 'ブログ記事の編集ページの設定',
+    'Settings for Page'  => 'ウェブページの編集ページの設定',
 );
 
 1;
